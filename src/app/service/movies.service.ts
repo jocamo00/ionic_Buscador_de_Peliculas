6054306963da08df77c52bd6a8b952bf0ebfb08e
Jose Carreres <jose.carreres.81@gmail.com>
2020-07-31 16:06:52 +0200

    Usando el servicio getUpcoming() para mostrar un slider con los proximos estrenos (tab1.page.ts y tab1.page.html)

--- src/app/service/movies.service.ts
+++ src/app/service/movies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { RespuestaMDB } from '../interfaces/interfaces';
 
 const URL = environment.url;
 const apiKey = environment.apiKey;
@@ -14,7 +15,7 @@ export class MoviesService {
 
   getUpcoming() {
 
-    return this.http.get(`${URL}/movie/upcoming?api_key=${apiKey}&language=es&page=1&region=es`);
+    return this.http.get<RespuestaMDB>(`${URL}/movie/upcoming?api_key=${apiKey}&language=es&page=1&region=es`);
 
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../service/movies.service';
+import { Pelicula } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  upcomingMovies: Pelicula[] = [];
+
+  slideOpts = {
+    slidesPerView: 1.3,
+    freeMode: true
+  };
+
+  constructor( private moviesService: MoviesService ) {}
+
+  ngOnInit() {
+    this.moviesService.getUpcoming()
+    .subscribe( resp => {
+      console.log('Resp', resp);
+      this.upcomingMovies = resp.results;
+    });
+  }
 
 }
