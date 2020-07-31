656e485cbfa5fd6f41f5e8cc3c94b2851693b650
Jose Carreres <jose.carreres.81@gmail.com>
2020-07-31 16:59:08 +0200

    Usando el servicio gerPopular() para mostrar un slider con las peliculas mas populares

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,19 +10,33 @@ import { Pelicula } from '../interfaces/interfaces';
 export class Tab1Page implements OnInit{
 
   upcomingMovies: Pelicula[] = [];
+  popularMovies: Pelicula[] = [];
 
   slideOpts = {
     slidesPerView: 1.3,
-    freeMode: true
+    freeMode: true,
+    spaceBetween: -10
   };
 
   constructor( private moviesService: MoviesService ) {}
 
   ngOnInit() {
+    this.getUpcoming();
+    this.getpopular();
+  }
+
+  getUpcoming() {
     this.moviesService.getUpcoming()
     .subscribe( resp => {
-      console.log('Resp', resp);
       this.upcomingMovies = resp.results;
+    });
+  }
+
+  getpopular() {
+    this.moviesService.getPopular()
+    .subscribe( resp => {
+      console.log('populares', resp);
+      this.popularMovies = resp.results;
     });
   }
 
