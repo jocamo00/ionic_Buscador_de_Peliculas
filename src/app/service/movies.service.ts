efa3c0e676179413b42b370d59d1035da4cb252a
Jose Carreres <jose.carreres.81@gmail.com>
2020-07-31 16:19:24 +0200

    Se crea el metodo getPopular() en el servicio para obtener las peliculas populares

--- src/app/service/movies.service.ts
+++ src/app/service/movies.service.ts
@@ -16,6 +16,10 @@ export class MoviesService {
   getUpcoming() {
 
     return this.http.get<RespuestaMDB>(`${URL}/movie/upcoming?api_key=${apiKey}&language=es&page=1&region=es`);
+  }
 
+  getPopular() {
+
+    return this.http.get<RespuestaMDB>(`${URL}/movie/popular?api_key=${apiKey}&language=es&page=1`);
   }
 }
